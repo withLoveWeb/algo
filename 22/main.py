1f7da344c613b88527c03691faa0ee50e593f720
baxrax <withloveweb7@gmail.com>
2024-05-19 22:45:48 +0300

    try to solve 22

---
+++ 22/main.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        bracket = "()"*n
+        print(bracket)
+        res = []
+
+        
+
+        print(res)
+
+
+
+
+s = Solution()
+print(s.generateParenthesis(2))
