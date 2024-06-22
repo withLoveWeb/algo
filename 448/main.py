3adb396360c48ec7b84a31620258da0c6d46c5e4
baxrax <withloveweb7@gmail.com>
2024-06-22 13:50:10 +0300

    solve 448

--- 448/main.py
+++ 448/main.py
@@ -6,21 +6,20 @@ from typing import List
 
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
         nums = sorted(set(nums))
-        tmp = nums[0]
         i = 0
         ans = []
-        while tmp != nums[-1]:
-            if num
-        # for i in range(len(nums)):
-        #     if tmp == nums[i]:
-        #         tmp += 1
-        #     else:
-        #         ans.append(tmp)
-        #         if nums[i+1]
-        #         tmp = nums[i]
+        for tmp in range(1,n+1):
+            try:
+                if nums[i] == tmp:
+                    i += 1
+                else:
+                    ans.append(tmp)
+            except IndexError:
+                ans.append(tmp)
         return ans
     
 s = Solution()
-print(s.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
+print(s.findDisappearedNumbers([1,1,2]))
 
